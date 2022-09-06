7c97d89a5c74f3ab4c57fd0e09b2d8821766c256
wcgordon1 <wcgordon1@gmail.com>
2022-09-05 22:27:28 -0700

    refactored render function, may add old leads, almost done, going to add save current tab and export to excel:

--- app.js
+++ app.js
@@ -2,31 +2,51 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+// 1. Grab the SAVE TAB button and store it in a tabBtn variable
+const tabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value)
-    inputEl.value = ''
-    renderLeads()
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+const tabs = [
+    { url: "https://www.linkedin.com/in/per-harald-borgen/" }
+]
+
+tabBtn.addEventListener("click", function () {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
 
 })
 
-//document.createElement('li') << Other way to append html elements 
-//li.textContent = myLeads[i]
-//ulEl.append(li)
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        //Use template strings. code below is too messy
-        //listItems += '<li> <a target="_blank" href="' + myLeads[i] + myLeads[i] + '" ></a></li>'
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
-            </a>
-        </li>
-    `
-
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
     }
-
     ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
